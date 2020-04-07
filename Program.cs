670e91322adf4d16c898382106430fd2a4ab1d8f
pamaf1 <61753383+pamaf1@users.noreply.github.com>
2020-04-08 00:12:20 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Лаба_4_2._2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bird b = new Bird();
+            b.DisplayInfo();
+            b.HightFly();
+
+            Owl o = new Owl();
+            o.DisplayInfo();
+
+            Dog d = new Dog();
+            d.DisplayInfo();
+        }
+    }
+    public abstract class Animal
+    {
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Sound { get; set; }
+
+        public int Speed { get; set; }
+
+        public abstract void HightFly();
+
+        public Animal(string name, int age, string sound, int speed)
+        {
+            Name = name;
+            Age = age;
+            Sound = sound;
+            Speed = speed;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Имя: {Name}\nВозраст: {Age}\nЗвук: {Sound}\nСкорость: {Speed}\n");
+        }
+
+    }
+    public class Bird : Animal
+    {
+
+        public Bird() : base("Bird", 7, "Кря", 50) { }
+
+        public override void HightFly()
+        {
+            Console.WriteLine("Висота польоту птаха: 70");
+        }
+
+
+
+    }
+    public class Dog : Animal
+    {
+
+        public Dog() : base("Dog", 10, "Гаф", 30) { }
+
+        public override void HightFly()
+        {
+            Console.WriteLine("Только бегает");
+        }
+
+    }
+    public class Owl : Animal
+    {
+
+        public Owl() : base("Owl", 20, "Угу", 40) { }
+
+        public override void HightFly()
+        {
+            Console.WriteLine("30");
+        }
+
+    }
+}
